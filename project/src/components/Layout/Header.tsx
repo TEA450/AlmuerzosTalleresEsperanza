c11c554711f820f22400900dd4fd5f2804804d95
MPaulaR02 <158857719+MPaulaR02@users.noreply.github.com>
2025-09-16 22:49:20 -0500

    logo not visible on cell phones

--- project/src/components/Layout/Header.tsx
+++ project/src/components/Layout/Header.tsx
@@ -12,7 +12,7 @@ const Header: React.FC = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-28">
           {/* Logo */}
-          <div className="flex items-center space-x-4">
+          <div className="hidden sm:flex items-center space-x-4">
             <div className="flex items-center">
               <img 
                 src="/images/logo.gif" 
